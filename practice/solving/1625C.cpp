91f77f3abde4710683c1453449fa623d11fd0e21
freddie <freddie.mao@gmail.com>
2022-01-18 18:48:32 +0000

    1625C nearly done

--- practice/solving/1625C.cpp
+++ practice/solving/1625C.cpp
@@ -5,11 +5,13 @@ using namespace std;
 // What if there is a vector for each point and there is vector of pairs for the number of removals
 // I have an error somewhere and it's not integer overflow
 // Keeps screwing up on test case 8, idk what the issue is as the code works fine for other tests
+// Basically, some states that have lower speed and larger time are better than states with higher speed and lower time
+// Need to make it consider these states, time complexity should go to O(n^3)
 
 const int mxN = 500;
 const ll INF = 1000000000000000ll;
 // Vector for all of the points, map for the number of removals done
-vector<map<ll,vector<ll>>> dp(mxN+1);
+vector<map<ll,pair<ll,ll>>> dp(mxN+1);
 
 void solve() {
     ll n, l, k;
@@ -21,44 +23,34 @@ void solve() {
         cin >> limit[i];
     coords[n] = l;
 
-    vector<ll> fill;
-    fill.push_back(INF);
-    fill.push_back(INF);
-    for (int i = 0; i <= n; i++) {
+    // This initally sets the distance to every state to infinity
+    pair<ll,ll> fill = {INF,INF};
+    for (int i = 0; i <= n; i++)
         for (int j = 0; j <= k; j++)
             dp[i][j] = fill;
-    }
     
     // This is here as the first sign can't be removed
-    vector<ll> start;
-    start.push_back(limit[0]*coords[1]);
-    start.push_back(limit[0]);
-    dp[1][0] = start;
-    for (int i = 1; i < n; i++) {
+    dp[0][0] = {0,limit[0]};
+    for (int i = 0; i < n; i++) {
         // This goes over all of the possible choices that could've been made
-        for (int j = 0; j < (int) dp[i].size(); j++) {
+        for (int j = 0; j <= k; j++) {
             // This is to skip the current road sign
             if (j < k) {
-                vector<ll> next;
-                next.push_back(dp[i][j][0] + (coords[i+1]-coords[i])*dp[i][j][1]);
-                next.push_back(dp[i][j][1]);
-                if (next[0] < dp[i+1][j+1][0])
-                    dp[i+1][j+1] = next;
+                if (dp[i][j].first + (coords[i+1]-coords[i])*dp[i][j].second < dp[i+1][j+1].first)
+                    dp[i+1][j+1] = {dp[i][j].first + (coords[i+1]-coords[i])*dp[i][j].second,dp[i][j].second};
             }
             // This is to not skip the current sign
-            vector<ll> otherNext;
-            otherNext.push_back(dp[i][j][0] + (coords[i+1]-coords[i])*limit[i]);
-            otherNext.push_back(limit[i]);
-            if (otherNext[0] < dp[i+1][j][0])
-                dp[i+1][j] = otherNext;
+            if (dp[i][j].first + (coords[i+1]-coords[i])*limit[i] < dp[i+1][j].first)
+                dp[i+1][j] = {dp[i][j].first + (coords[i+1]-coords[i])*limit[i],limit[i]};
         }
     }
 
     // This iterates over all of the totals for every number of signs removed, getting the lowest
     ll out = INF;
     for (int i = 0; i <= k; i++)
-        out = min(out, dp[n][i][0]);
-    cout << out << "\n";
+        out = min(out, dp[n][i].first);
+    
+    std::cout << out << "\n";
 }
 
 int main(void) {
